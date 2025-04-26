5c25307e591a6fc2cc01684bda764f3ec6af3cce
felipe-py <luis.silvareal77@gmail.com>
2025-04-25 22:33:23 -0300

    Adicionada rota de login

---
+++ src/infra/api/express/routes/professor/login-professor.express.route.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import { loginProfessorUsecase } from "../../../../../usecases/professorUseCase/login-professor/login-professor.usecase";
+import { HttpMethod, Route } from "../route";
+
+export type LoginProfessorResponseDto = {
+    nome: string;
+    email: string;
+}
+
+export class LoginProfessorRoute implements Route {
+
+    private constructor (
+        private readonly path: string,
+        private readonly method: HttpMethod,
+        private readonly loginProfessorService: loginProfessorUsecase
+    ) {}
+
+    public static create (loginProfessorService: loginProfessorUsecase): LoginProfessorRoute {
+        return new LoginProfessorRoute(
+            "/professor/login",
+            HttpMethod.POST,
+            loginProfessorService
+        );
+    }
+
+    public getHandler() {
+        return async (request: Request, response: Response) => {
+            const { email, senha } = request.body;
+
+            const input = {
+                email,
+                senha,
+            };
+
+            const output: LoginProfessorResponseDto = 
+                await this.loginProfessorService.execute(input);
+
+            const responseBody = this.present(output);
+
+            response.status(200).json(responseBody);
+
+        };
+    }
+
+    public getPath(): string {
+        return this.path;
+    }
+
+    public getMethod(): HttpMethod {
+        return this.method;
+    }
+
+    private present(input: LoginProfessorResponseDto): LoginProfessorResponseDto {
+        const response = {
+            nome: input.nome,
+            email: input.email,
+        }
+        return response;
+    }
+
+}
