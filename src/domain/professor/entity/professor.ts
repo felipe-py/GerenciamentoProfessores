0ffe32ece8e1a74ab84cb61742e1b51326de6495
felipe-py <luis.silvareal77@gmail.com>
2025-04-26 21:20:03 -0300

    Adicionando validações para atributos do professor

--- src/domain/professor/entity/professor.ts
+++ src/domain/professor/entity/professor.ts
@@ -12,10 +12,16 @@ export class Professor {
     private constructor(private props: ProfessorProps) {}
 
     public static create(cpf: string, nome: string, email: string, senha: string) {
+       
+        this.validadeCpf(cpf);
+        this.validadeNome(nome);
+        this.validadeEmail(email);
+        this.validadeSenha(senha);
+
         return new Professor({
             id: crypto.randomUUID().toString(),
             cpf,
-            nome,              // CRIPTOGRAFAR CPF, EMAIL, SENHA
+            nome,              
             email, 
             senha,
         });
@@ -44,5 +50,54 @@ export class Professor {
     public get senha() {
         return this.props.senha;
     }
+
+    private static validadeCpf(cpf: string) : void {
+        if (!cpf) {
+            throw new Error("CPF não pode ser vazio");
+        }
+
+        if (cpf.length !== 11) {
+            throw new Error("CPF deve ter 11 dígitos");
+        }
+
+        if (!/^\d+$/.test(cpf)) {
+            throw new Error("CPF deve conter apenas números");
+        }
+    }
+
+    private static validadeNome(nome: string) : void {
+        if (!nome) {
+            throw new Error("Nome não pode ser vazio");
+        }
+
+        if (nome.length < 3) {
+            throw new Error("Nome deve ter pelo menos 3 caracteres");
+        }
+
+        if (!/^[a-zA-Z\s]+$/.test(nome)) {
+            throw new Error("Nome deve conter apenas letras e espaços");
+        }
+    }
+
+    private static validadeEmail(email: string) : void {
+        if (!email) {
+            throw new Error("Email não pode ser vazio");
+        }
+
+        if (email.length < 5) {
+            throw new Error("Email deve ter pelo menos 5 caracteres");
+        }
+    }
+
+    private static validadeSenha(senha: string) : void {
+        if (!senha) {
+            throw new Error("Senha não pode ser vazia");
+        }
+
+        if (senha.length !== 4) {
+            throw new Error("Senha deve ter 4 caracteres");
+        }
+    }
+
 
 }
